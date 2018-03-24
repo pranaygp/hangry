ab82e0aeaebda4c594639f3de00ea726504bdaa6
Abdullahi Abdalla <abdalla6@illinois.edu>
2018-03-24 13:45:28 -0500

    Implemented some basic location endpoints

---
+++ api/views/locations.py
@@ -0,0 +1,93 @@
+import os
+from api import app
+from api import db
+from api.models import User
+from api.utils import APIError
+from flask import Blueprint, request
+from flask import jsonify
+from flask_login import LoginManager, login_required, login_user, logout_user
+
+engine = db.engine
+conn = engine.connect()
+
+mod = Blueprint('locations', __name__)
+
+@mod.route('/locations', methods = ["GET"])
+def get_all_locations():
+    try:
+        result = conn.execute("SELECT * FROM location")
+        locations = []
+        for row in result:
+            location = {}
+            location["location_id"] = row["location_id"]
+            location["restaurant_id"] = row["restaurant_id"]
+            location["address"] = row["address"]
+            location["zipcode"] = row["zipcode"]
+            location["city"] = row["city"]
+            location["country"] = row["country"]
+            location["latitude"] = row["latitude"]
+            location["longitude"] = row["longitude"]
+            locations.append(location)
+        return jsonify({'status':'success', 'locations' : locations})
+    except Exception as e:
+        raise APIError(str(e))
+
+@mod.route('/locations/restaurant/<restaurant_id>', methods = ["GET"])
+def get_all_restaurant_locations(restaurant_id):
+    try:
+        result = conn.execute("SELECT * FROM location WHERE restaurant_id = {}".format(restaurant_id))
+        locations = []
+        for row in result:
+            location = {}
+            location["location_id"] = row["location_id"]
+            location["restaurant_id"] = row["restaurant_id"]
+            location["address"] = row["address"]
+            location["zipcode"] = row["zipcode"]
+            location["city"] = row["city"]
+            location["country"] = row["country"]
+            location["latitude"] = row["latitude"]
+            location["longitude"] = row["longitude"]
+            locations.append(location)
+        return jsonify({'status':'success', 'locations' : locations})
+    except Exception as e:
+        raise APIError(str(e))
+
+@mod.route('/locations/zipcode/<zipcode>', methods = ["GET"])
+def get_all_zipcode_locations(zipcode):
+    try:
+        result = conn.execute("SELECT * FROM location WHERE zipcode = \'{}\'".format(zipcode))
+        locations = []
+        for row in result:
+            location = {}
+            location["location_id"] = row["location_id"]
+            location["restaurant_id"] = row["restaurant_id"]
+            location["address"] = row["address"]
+            location["zipcode"] = row["zipcode"]
+            location["city"] = row["city"]
+            location["country"] = row["country"]
+            location["latitude"] = row["latitude"]
+            location["longitude"] = row["longitude"]
+            locations.append(location)
+        return jsonify({'status':'success', 'locations' : locations})
+    except Exception as e:
+        raise APIError(str(e))
+
+@mod.route('/locations/city/<city>', methods = ["GET"])
+def get_all_city_locations(city):
+    try:
+        result = conn.execute("SELECT * FROM location WHERE city = \'{}\'".format(city))
+        locations = []
+        for row in result:
+            location = {}
+            location["location_id"] = row["location_id"]
+            location["restaurant_id"] = row["restaurant_id"]
+            location["address"] = row["address"]
+            location["zipcode"] = row["zipcode"]
+            location["city"] = row["city"]
+            location["country"] = row["country"]
+            location["latitude"] = row["latitude"]
+            location["longitude"] = row["longitude"]
+            locations.append(location)
+        return jsonify({'status':'success', 'locations' : locations})
+    except Exception as e:
+        raise APIError(str(e))
